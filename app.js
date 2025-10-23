d5fbbfc690aca73053cd71bc4a1f664351b50955
Ivan-Henrique <domingues.gilvan@escola.pr.gov.br>
2025-10-23 09:09:19 -0300

    save

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 let numeroSecreto = 7;
 
-function exebirTextoNatela('tag'){
+function exebirTextoNatela(tag){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 exebirTextoNatela('h1','Jogo do numero secreto');
-exebirTextoNatela('p','Vocẽ descobriu o numero secreto!');
+exebirTextoNatela('p','Vocẽ descobriu o numero secreto com 5 tentativas');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto){
       exebirTextoNatela('h1','acertou!')
-      exebirTextoNatela('p1','voce descobriu o numero secreto! ')
+      let mensagemTentativas = `Vocẽ descobriu o numero secreto com 5 tentativas´
+      
+      exebirTextoNatela('p1','Vocẽ descobriu o numero secreto com 5 tentativas')
+
     } else {
   if (chute > numeroSecreto){
       exebirTextoNatela('p','O numero secreto é menor');
@@ -28,7 +31,6 @@ function verificarChute(){
 function gerarNumeroAleatorio(){
   return parseInt  (mach.random() * 10 + 1);
 }
-
 
 
 
