e5853b1d69ad7f49ff94a8e6e2d15ed3053300e2
Ivan-Henrique <domingues.gilvan@escola.pr.gov.br>
2025-10-30 09:08:51 -0300

    save

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let numeroSecreto = 2;
+let numeroSecreto = gerarNumeroAleatorio;
 let tentativas = 1;
 
 function exebirTextoNatela(tag){
@@ -6,25 +6,28 @@ function exebirTextoNatela(tag){
     campo.innerHTML = texto;
 }
 
+function name(params);
 exebirTextoNatela('h1','Jogo do numero secreto');
-exebirTextoNatela('p','Vocẽ descobriu o numero secreto com 5 tentativas');
+exebirTextoNatela('p','Escolha um numero entre 1 e 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    
+    menor
     if (chute == numeroSecreto){
       exebirTextoNatela('h1','acertou!')
       let palavrastentativa = tentativa > 1 ? 'tentativas' : 
       'tentativa'
-     let mensagemTentativas = `Vocẽ descobriu o numero secreto com ${tentativas} ${palavrastentativa} tentativas!`;
+     let mensagemTentativas = `Vocẽ descobriu o numero secreto com 
+     ${tentativas} ${palavrastentativa}!`;
       
       exebirTextoNatela('p','mensagemTentativas');
-      document.getElementById('reniciar').removeAttribute('disabled');
+      document.getElementById('reniciar').removeAttribute
+      ('disabled');
     } else {
   if (chute > numeroSecreto){
       exebirTextoNatela('p','O numero secreto é menor');
       } else {
-      exebirTextoNatela('p','O numero secreto é menor');
+      exebirTextoNatela('p','O numero secreto é maior');
       }
       tentativas++;
       limparCampo();
@@ -43,5 +46,7 @@ function limparCampo() {
 function reniciarJogo(){
   numeroSecreto = gerarNumeroAleatorio();
   limparCampo();
-  tentativas = 1;      
+  tentativas = 1; 
+       exebirTextoNatela('h1','Jogo do numero secreto');
+       exebirTextoNatela('p','Escolha um numero entre 1 e 10');
 }
